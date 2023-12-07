d8197bca1591e1e2b139b964114fef66895247bd
kochan4php <aprodeosubarno@gmail.com>
2023-12-07 23:06:44 +0700

    string_interpolation.rb

---
+++ belajar-ruby-dasar/string_interpolation.rb
@@ -0,0 +1,12 @@
+# ! String interpolation merupakan suatu cara menyisipkan variable atau expression apapun ke dalam string.
+# ! String interpolation dapat dilakukan dengan menggunakan tanda pagar diikuti dengan kurung kurawal (`#{variable / expression}`).
+
+# contoh jika menggunakan variable:
+nama = "Deo"
+umur = 18
+puts "Halo, nama saya #{nama}, umur saya #{umur} tahun."
+
+# contoh jika menggunakan expression:
+a = 5
+b = 10
+puts "Hasil penjumlahan dari #{a} + #{b} adalah #{a + b}."
