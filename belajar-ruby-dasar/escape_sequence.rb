c48d711701c76be4944026b1850a566976bf2eb8
kochan4php <aprodeosubarno@gmail.com>
2023-12-08 23:39:20 +0700

    escape_sequence.rb

---
+++ belajar-ruby-dasar/escape_sequence.rb
@@ -0,0 +1,18 @@
+# ! Sama seperti C, Ruby mendukung adanya karakter escape sequence yang dapat disisipkan ke dalam string.
+
+# ! Berikut ini adalah daftar karakter escape sequence yang didukung oleh Ruby:
+# * \n: karakter newline
+# * \r: karakter carriage return
+# * \t: karakter tab
+# * \v: karakter vertical tab
+# * \b: karakter backspace
+# * \' atau \": karakter single quote atau double quote
+# * \\: karakter backslash
+
+puts "Hello\nRuby"
+puts "=" * 10
+puts "Hello\rRuby"
+puts "=" * 10
+puts "Hello\tRuby"
+
+# dll
