ee78044116322489454e48bea71cc4fb8fc72752
kochan4php <aprodeosubarno@gmail.com>
2023-12-08 23:42:51 +0700

    boolean.rb

---
+++ belajar-ruby-dasar/boolean.rb
@@ -0,0 +1,12 @@
+# ! Boolean merupakan tipe data yang hanya memiliki dua nilai, yaitu true dan false.
+# ! Dalam Ruby, true merupakan hasil instansiasi dari class TrueClass, sedangkan false merupakan hasil instansiasi dari class FalseClass.
+
+# contoh
+benar = true
+salah = false
+
+puts benar
+puts salah
+
+puts benar.class()
+puts salah.class()
