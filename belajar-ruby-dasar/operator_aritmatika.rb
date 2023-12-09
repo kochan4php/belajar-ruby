bf6951c1d17422381beddf94e554d3ad1007b6e4
kochan4php <aprodeosubarno@gmail.com>
2023-12-10 00:01:54 +0700

    operator_aritmatika.rb

---
+++ belajar-ruby-dasar/operator_aritmatika.rb
@@ -0,0 +1,22 @@
+# ! Operator aritmatika adalah operator yang digunakan untuk melakukan operasi matematika.
+
+# ! Macam-macam operator aritmatika
+#* (+) => penjumlahan
+#* (-) => pengurangan
+#* (*) => perkalian
+#* (/) => pembagian
+#* (%) => modulus / sisa bagi
+#* (**) => eksponen / perpangkatan
+
+# contoh
+a = 2
+b = 4
+
+puts b + a
+puts b - a
+puts b * a
+puts b / a
+puts b % a
+puts b ** a
+
+# ! Note: semua operasi aritmatika pada Ruby, jika angka yang dilakukan operasi merupakan angka Integer / bilangan bulat, maka hasil dari operasi tersebut akan menghasilkan bilangan bulat juga, termasuk dengan pembagian. Kecuali jika angka yang dilakukan operasi merupakan angka Float / bilangan rill, maka hasil dari operasi tersebut akan menghasilkan bilangan rill juga. 
