02419dc04b602707377053bac18b56261ee93732
kochan4php <aprodeosubarno@gmail.com>
2023-12-10 00:17:40 +0700

    update operator_aritmatika.rb

--- belajar-ruby-dasar/operator_aritmatika.rb
+++ belajar-ruby-dasar/operator_aritmatika.rb
@@ -19,4 +19,4 @@ puts b / a
 puts b % a
 puts b ** a
 
-# ! Note: semua operasi aritmatika pada Ruby, jika angka yang dilakukan operasi merupakan angka Integer / bilangan bulat, maka hasil dari operasi tersebut akan menghasilkan bilangan bulat juga, termasuk dengan pembagian. Kecuali jika angka yang dilakukan operasi merupakan angka Float / bilangan rill, maka hasil dari operasi tersebut akan menghasilkan bilangan rill juga. 
+# ! Note: semua operasi aritmatika pada Ruby, jika angka yang dilakukan operasi merupakan angka Integer / bilangan bulat, maka hasil dari operasi tersebut akan menghasilkan bilangan bulat juga, termasuk dengan pembagian. Sedangkan jika angka yang dilakukan operasi merupakan angka Float / bilangan rill, maka hasil dari operasi tersebut akan menghasilkan bilangan rill juga. 
