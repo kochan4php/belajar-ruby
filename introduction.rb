09cc34fc140a441d6fedb7c3296a444630aaef1f
kochan4php <aprodeosubarno@gmail.com>
2023-12-10 18:03:41 +0700

    add introduction

---
+++ introduction.rb
@@ -0,0 +1,10 @@
+my_self = {
+    name: "Deo Subarno",
+    nickname: "Kochan",
+    pronouns: "He/Him",
+    skils: ["JS", "TS", "PHP", "Ruby", "MERN Stack", "Next.js", "Nest.js", "Laravel", "Docker", "MySQL"],
+    role: "Backend Engineer",
+    say_hello: lambda { puts "Hello, my name is #{my_self[:name]}. You can call me #{my_self[:nickname]}" }
+}
+
+my_self[:say_hello].call()
