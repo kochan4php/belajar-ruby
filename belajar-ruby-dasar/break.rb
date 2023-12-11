cb2547719f0a6bc7fdec21eed8d6e4031cfa2e7f
kochan4php <aprodeosubarno@gmail.com>
2023-12-11 22:45:00 +0700

    break.rb

---
+++ belajar-ruby-dasar/break.rb
@@ -0,0 +1,20 @@
+# ! Perintah break digunakan untuk menghentikan proses pengulangan secara paksa, meskipun condition yang didefinisikan masih bernilai true.
+
+# contoh menggunakan while
+index = 0
+while index < 10
+    puts "Pengulangan ke-#{index}"
+    break if index == 4
+    index += 1
+end
+
+puts "=" * 30
+
+# contoh menggunakan for
+for item in 1...11
+    puts item
+    
+    if item == 3
+        break
+    end
+end
