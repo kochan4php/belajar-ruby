d7a2855fbdd39fbd5b88474c7db16a7f155a210e
kochan4php <aprodeosubarno@gmail.com>
2023-12-21 23:08:16 +0700

    update

--- introduction.rb
+++ introduction.rb
@@ -1,10 +1,10 @@
 my_self = {
-    name: "Deo Subarno",
-    nickname: "Kochan",
-    pronouns: "He/Him",
-    skils: ["JS", "TS", "PHP", "Ruby", "MERN Stack", "Next.js", "Nest.js", "Laravel", "Docker", "MySQL"],
-    role: "Backend Engineer",
-    say_hello: -> { "Hello, my name is #{my_self[:name]}. You can call me #{my_self[:nickname]}" }
+  name: 'Deo Subarno',
+  nickname: 'Kochan',
+  pronouns: 'He/Him',
+  skils: ['JS', 'TS', 'PHP', 'Ruby', 'MERN Stack', 'Next.js', 'Nest.js', 'Laravel', 'Docker', 'MySQL'],
+  role: 'Backend Engineer',
+  say_hello: -> { "Hello, my name is #{my_self[:name]}. You can call me #{my_self[:nickname]}" }
 }
 
-puts my_self[:say_hello].call()
+puts my_self[:say_hello].call
